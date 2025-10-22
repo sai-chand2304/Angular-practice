dfd5482dcab255e369628eadda8b78abb7c25858
sai-chand <saichand1212@gmail.com>
2025-10-22 11:21:59 +0530

    parent-child comms

--- practice/src/app/app.routes.ts
+++ practice/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'parent',
+        loadComponent: () => import('./components/parent/parent.component').then(m => m.ParentComponent)
+    }
+];
--- practice/src/app/components/parent/parent.component.ts
+++ practice/src/app/components/parent/parent.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [],
+  imports: [ChildComponent],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
 export class ParentComponent {
 
+  message:string="Hi, message in parent component";
 }
