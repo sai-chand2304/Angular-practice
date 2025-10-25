e3d0d09f5b63fdae7151fc67c86925019d610149
sai-chand <saichand1212@gmail.com>
2025-10-25 12:42:05 +0530

    added home component

--- practice/src/app/app.routes.ts
+++ practice/src/app/app.routes.ts
@@ -4,5 +4,14 @@ export const routes: Routes = [
     {
         path:'parent',
         loadComponent: () => import('./components/parent/parent.component').then(m => m.ParentComponent)
+    },
+    {
+        path:'home',
+        loadComponent:()=>import('./home/home.component').then(m=>m.HomeComponent)
+    },
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
     }
 ];
